35033224baed2cf9fd3575c9e6be6b50375f96b3
sputnicc <nicolasdiaz7171@gmail.com>
2020-08-15 22:31:44 -0700

    8/15

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from random_lists import *
 import random
 
 
+
 # Opens pokemon_stats.json, creates a Pokemon object for each pokemon, stores all pokemon objects into 'pokemons{}'
 with open('pokemon_stats.json') as json_file:
     pokemon_data = json.load(json_file) 
@@ -12,7 +13,7 @@ with open('pokemon_stats.json') as json_file:
 
     # For pokemon in the json file, create Pokemon Object
     for poke in pokemon_data:
-        pokemons[poke['name']] = Pokemon(poke['name'],poke['level'], poke['type'], poke['max_health'], poke['current_health'], poke['ko'] )
+        pokemons[poke['name']] = Pokemon(poke['name'],poke['level'], poke['type'], poke['max_health'], poke['current_health'], poke['ko'], poke['exp'] )
 
 # Opens trainer_log.json, creates a Trainer object for each trainer, stores all trainer objects into 'trainers{}'
 with open('trainer_log.json') as json_file:
@@ -122,48 +123,97 @@ async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency*1000)}ms')
 
 @client.command() # Trainer command to attack another trainer
-async def battle(ctx, loser):
+async def start_battle(ctx, loser):
+    global LAST_TURN
+    global START_BATTLE 
 
-    try: 
-        battler = ctx.author.name
-        await ctx.send(trainers[battler].attack(trainers[loser]))
+    try:
+        if START_BATTLE == 1:
+            return await ctx.send('Please wait for the current battle to finish before starting a new battle')
 
     except:
-        await ctx.send('You cannot battle this person!')
+        try: 
+            
+            battler = ctx.author.name
+            LAST_TURN = battler
+            START_BATTLE = 1
+            await ctx.send(f'----- {(trainers[battler].name).upper()} has decided to battle {(trainers[loser].name).upper()}! -----')
+            await ctx.send(trainers[battler].attack(trainers[loser]))
+            print(trainers[battler].pokemon_list[0])
 
-@client.command()
-async def attack_sean(ctx):
-    await ctx.send(f'{ctx.author.name} is dead')
+        except:
+            await ctx.send('You cannot battle this person!')
+
+@client.command() # Trainer command to attack another trainer
+async def battle(ctx, loser):
+    global LAST_TURN
+    global START_BATTLE 
+
+    try:
+        if START_BATTLE == 1: 
+            battler = ctx.author.name
+            
+            if ctx.author.name != LAST_TURN: 
+                await ctx.send(trainers[battler].attack(trainers[loser]))
+                LAST_TURN = ctx.author.name
+
+            else:
+                return await ctx.send('It is not your turn!')
+
+            c1 = 0
+            c2 = 0
+            for pok in trainers[battler].pokemon_list:
+                if pok.ko == 1:
+                    c1 += 1
+                
+                if c1 == len(trainers[battler].pokemon_list):
+                    await ctx.send(f'\n\n---------- Congratulations {(trainers[loser].name).upper()} you have defeated {(trainers[battler].name).upper()} in battle! ----------\n\n')
+                    LAST_TURN = ''
+                    START_BATTLE = 0
+
+            for pok in trainers[loser].pokemon_list:
+                if pok.ko == 1:
+                    c2 += 1
+                
+                if c2 == len(trainers[loser].pokemon_list):
+                    await ctx.send(f'\n\n---------- Congratulations {(trainers[battler].name).upper()} you have defeated {(trainers[loser].name).upper()} in battle! ----------\n\n')
+                    LAST_TURN = ''
+                    START_BATTLE = 0
+        
+    except: 
+        return await ctx.send('Please start a battle with another trainer')
 
 @client.command() # Trainer command to switch to a different pokemon in your pokemon list
 async def switch_to(ctx, new_poke):
+    global LAST_TURN
     battler = ctx.author.name
     
-    await ctx.send(trainers[battler].switch_pokemon(new_poke))
-
-
-
-@client.command() # Trainer command to display the state of your current pokemons 
-async def my_status(ctx):
-    battler = ctx.author.name
+    if ctx.author.name != LAST_TURN: 
+        return await ctx.send(trainers[battler].switch_pokemon(new_poke))
     
-    await ctx.send(trainers[battler].pokemon_status())
+    else: 
+        return await ctx.send('It is not your turn!')
 
 
+@client.command() # Trainer command to display Trainer summaries (detaults to your summary)
+async def status(ctx, *battler):
+    try: 
+        await ctx.send(trainers[battler[0]].pokemon_status())
 
+    except:
+        battler = ctx.author.name
+        await ctx.send(trainers[battler].pokemon_status())
 
+@client.command()
+async def potion(ctx, poke):
+    global LAST_TURN 
+    battler = ctx.author.name
 
-
-
-
-
-
-
-
-
-
-
-
+    if ctx.author.name != LAST_TURN: 
+        await ctx.send(trainers[battler].use_potion(poke))
+    
+    else: 
+        return await ctx.send('It is not your turn!')
 
 # -----------------------------------------------------------------------
 # Misc Bot commands
@@ -197,3 +247,28 @@ client.run('')
 
 
 
+"""
+
+How to Play: 
+
+    Commands: 
+
+        .status
+            - Gives you a summary of a Trainer's account
+                - Ex: 
+                    - .status => Returns summary of your Trainer's account (default)
+                    - .status Saph => Returns a summar of Saph's Trainer account
+
+        .battle XXXX
+            - Starts a battle with XXXX (where XXXX is a trainer's name)
+                - Ex: 
+                    - .battle Saph => Enters a battle between you and Saph
+
+        .switch_to XXXX
+            - Switches your currently active Pokemon to XXXX (where XXXX is a name from a Pokemon in your Trainer's pokemon list)
+
+
+
+
+
+"""
--- pokemon.py
+++ pokemon.py
@@ -1,49 +1,59 @@
 import random
 
 class Pokemon():
-    def __init__(self, name, level, type, max_health, current_health, ko):
+    def __init__(self, name, level, type, max_health, current_health, ko, exp):
         self.name = name
         self.level = level
         self.type = type
         self.max_health = max_health
         self.current_health = current_health
         self.ko = ko
-        # self.exp = exp
+        self.exp = exp
 
 
     def lose_health(self, damage):
         self.current_health -= damage
+        self.level_up(damage)
 
         if self.current_health <= 0:
-            return self.knock_out()
+            return self.knock_out(damage)
 
-        return '{name} now has {health} health!'.format(name=self.name, health = round(self.current_health, 2))
-
+        hit_string = '\nOUCH! {dam} damage was dealt to {name}'.format(name=self.name, dam=damage)
+        hit_string += '\n{name} now has {health}/{max_health} HP!'.format(name=(self.name).upper(), health = round(self.current_health, 2), max_health=self.max_health)
+        return hit_string
             
     def regaining_health(self):
-        if self.ko == 0:
-            self.current_health += 10
-            return '{name} now has {health} health!'.format(name=self.name, health = self.current_health)
+        POTION_STR = 10
 
-        if self.ko == 1: 
-            return '{name} has died, like died forreal forreal'.format(name=self.name)
+        if self.ko == 0: 
 
-    def knock_out(self):
+            if self.current_health + POTION_STR > self.max_health:
+                self.current_health = self.max_health
+
+            else:
+                self.current_health += POTION_STR
+
+            regaining_health_string =  '\n{name} has been healed and now has {health} health!'.format(name=(self.name).upper(), health = round(self.current_health, 2))
+            return regaining_health_string
+            
+        if self.ko == 1:
+            self.current_health = POTION_STR
+            regaining_health_string = '\n{name} has been REVIVED and now has {health} health!'.format(name=(self.name).upper(), health = round(self.current_health, 2))
+            self.ko = 0
+            return regaining_health_string
+
+    def knock_out(self, damage):
         self.ko = 1
-        return 'RIP {name} has died!!!'.format(name=self.name)
+        ko_string = '\nOUCH! {dam} damage was dealt to {name}'.format(name=self.name, dam=damage)
+        ko_string += '\nRIP {name} has died!!!'.format(name=self.name)
+        return ko_string
 
 
     def revive(self):
         self.ko = 0
         self.current_health = self.max_health
         print('{name} has been revived! Current health is {health}'.format(name=self.name, health = self.current_health))
-
-
-    """
-    def gain_exp(self):
-        self.exp = 0
-    """
-    
+ 
     
     def make_baby(self,mate):
         #at least one pokemon needs to be level 18 to reproduce
@@ -51,18 +61,58 @@ class Pokemon():
         
         if self.name == mate.name and (self.level >= 18 or mate.level >= 18):
             baby = Pokemon(self.name,1,self.type,self.max_health,self.max_health, 0)
-            print(f"Congrats, you are gonna be a father to a baby")
+            print(f"Congrats, you are gonna be a gender-neutral gaurdian to a baby")
             return baby
         else:
             print(f"These pokemon are not compatable")
+                # implement pokemon size comparison to determine coitus compatibility?
+
+    def level_up(self, damage):
+        # keeps track of pokemon level based on amount of accumulated experience
+        self.exp += round(damage * 3)
+        print(self)
+
+        min_level = 1
+        max_level = 100
+
+        possible_levels = list(range(min_level, max_level + 1))
+        experience_levels = []
+
+        for level in possible_levels:
+            if level <= 10:
+                experience_levels.append(level * 200)
+            elif level <= 20:
+                experience_levels.append(level * 2 * 200)
+            elif level <= 30:
+                experience_levels.append(level * 3 * 200)
+            else:
+                experience_levels.append(level * 4 * 200)
+
+        if self.exp < 80000:
+            level_position = [i for i,x in enumerate(experience_levels) if x <= self.exp < experience_levels[i + 1]]
+        else:
+            level_position = [99]
+
+        if len(level_position) == 0:
+            self.level = 1
+        else:
+            self.level = possible_levels[level_position[0]]
+        
+        return self.exp
+
+        # check level. if level changed, announce to trainer
+        # if level changes to 20, or 30, call evolution method 
+
+        # change pokemon health as level up
+        
 
     def evolution(self):
         #check if there is available evolution
+        
         #every pokemon should have evolution list?
         return     
             
     def attack(self, victim): 
-        
 
         if self.type == 'Fire':
             if victim.type == 'Grass': # x2
@@ -90,9 +140,9 @@ class Pokemon():
             else:
                 self.damage = (victim.max_health / 5) * round(1 + random.randrange(-40,40,1)/100, 2)
                 
-        attack_dialogue = '{attacker} has attacked {victim}!'.format(attacker=self.name, victim=victim.name)
-        lose_health_dialogue = victim.lose_health(round(self.damage, 2))
-        return attack_dialogue + '\n' + lose_health_dialogue 
+        lose_health_string = victim.lose_health(round(self.damage, 2))
+
+        return lose_health_string
 
 
 
@@ -103,87 +153,106 @@ class Trainer():
         self.potions = potions
         self.currently_active = currently_active
 
-    def use_potion(self):
-        if self.potions < 1:
-            print('{trainer} is out of potions!'.format(trainer=self.name))
- 
-        elif self.pokemon_list[self.currently_active].current_health < self.pokemon_list[self.currently_active].max_health:
-            self.potions -= 1
-            print('{trainer} used potion on {pokemon} ({potions} potions remaining)'.format(trainer=self.name, pokemon=self.pokemon_list[self.currently_active].name, potions=self.potions))
-            self.pokemon_list[self.currently_active].regaining_health()
+    def use_potion(self, poke):
 
-        else: 
-            print('{pokemon} is at max health!'.format(pokemon=self.pokemon_list[self.currently_active].name))
+        return_string = ''
+
+        if self.potions < 1:
+            return_string += '{trainer} is out of potions!'.format(trainer=(self.name).upper())
+            return return_string
+ 
+
+        for i in range(len(self.pokemon_list)): # checkint to see if pokemon exists in trainer's pokemon list
+            if self.pokemon_list[i].name == poke:
+                if self.pokemon_list[i].current_health < self.pokemon_list[i].max_health: # if the leath is less than max health.. use potion
+                    self.potions -= 1 
+                    regain_health_string = self.pokemon_list[i].regaining_health()
+
+                    return_string += regain_health_string
+                    return return_string
+                
+                else: # if pokemon is already at max health, dont' use potion
+                    return_string += '\n{pokemon} is at max health!'.format(pokemon=self.pokemon_list[i].name)
+                    return return_string
+
+        else: # pokemon not in list
+            return_string += 'Error, {pokemon} is not in your Pokemon list!'.format(pokemon=poke)
+            return return_string
+        
+        
+
 
     def attack(self, sufferer):
-        battle_dialogue = '{self} has decided to battle {sufferer}!'.format(self=self.name, sufferer=sufferer.name)
+        return_string = ''
+
+        if self.name == sufferer.name:
+            return_string += 'You cannot attack yourself!'.format(self=self.name)
+            return return_string
+
+
+        # Checking that Trainer Pokemons are ready for battle
+        if self.pokemon_list[self.currently_active].ko == 1:
+            return_string += 'You cannot go into battle with a dead pokemon. {self} please chane your currently active Pokemon'.format(self=self.name)
+            return return_string
 
         if sufferer.pokemon_list[sufferer.currently_active].ko == 1:
-            battle_dialogue_dead =  'You cannot attack a dead pokemon. {sufferer} please change your currently active Pokemon'.format(sufferer=sufferer.name)
-            return battle_dialogue_dead
+            return_string +=  'You cannot attack a dead pokemon. {sufferer} please change your currently active Pokemon'.format(sufferer=sufferer.name)
+            return return_string
 
-        return battle_dialogue + '\n' + self.pokemon_list[self.currently_active].attack(sufferer.pokemon_list[sufferer.currently_active])
+        # Intro Battle String
+        #return_string += '\n{self} has decided to battle {sufferer}!'.format(self=(self.name).upper(), sufferer=(sufferer.name).upper())
+        return_string += '\n{attacker} has attacked {victim}!'.format(attacker=(self.pokemon_list[self.currently_active].name).upper(), victim=(sufferer.pokemon_list[sufferer.currently_active].name).upper())
+
+        battle_event_string = self.pokemon_list[self.currently_active].attack(sufferer.pokemon_list[sufferer.currently_active])
+        return_string += battle_event_string
+
+        return return_string
 
 
     def switch_pokemon(self, new):
-
 
         for i in range(len(self.pokemon_list)):
             if self.pokemon_list[i].name == new:
                 self.currently_active = i
                 return '{self} is switching Pokemon to {new_pokemon}'.format(self=self.name, new_pokemon=self.pokemon_list[i].name)
 
-
         return 'Error: You do not own this pokemon'
 
         
     def pokemon_status(self):
         return_string = '------------------- Trainer {name} Summary ------------------- \n'.format(name=(self.name).upper())
+        return_string += '\n-------------------POKEMONS-------------------\n'
 
-        return_string += '\nPOKEMONS\n'
         for poke in self.pokemon_list:
-
             health_bar = '----------'
             percent_health = round((poke.current_health/poke.max_health)*10)
             
+            return_string += '\n- {name} '.format(name=((poke.name).upper()))
+            if self.pokemon_list[self.currently_active].name == poke.name:
+                return_string += ' *(currently active)*'
+            
+            if poke.ko == 1: # Health for dead pokemon
+                return_string += '\n--- Health [{health}] 0/{total} HP (DEAD)'.format(total=poke.max_health, health=health_bar)
 
-            if poke.ko == 1:
-                return_string += '- {name} [{health}] {current}/{total} HP (DEAD)'.format(name=(poke.name).upper(), current=round(poke.current_health, 2), total=poke.max_health, health=health_bar)
-
-                if self.pokemon_list[self.currently_active].name == poke.name:
-                    return_string += ' *currently active*'
-                return_string += '\n'
-
-            else:
+            else: # Health for alive pokemon
                 health_list = list(health_bar)
+
                 for i in range(percent_health):
                     health_list[i] = "+"
 
                 current_health_bar = "".join(health_list)
-            
+                return_string += '\n--- Health [{health}] {current}/{total} HP'.format(current=round(poke.current_health, 2), total=poke.max_health, health=current_health_bar)
 
+            return_string += '\n--- Type: {poke_type}'.format(poke_type=poke.type) # Type
+            return_string += '\n--- Level: {poke_lvl}'.format(poke_lvl=poke.level) # Level
+            return_string += '\n--- Total XP: {ex}'.format(ex=poke.exp)# XP til level up
+            return_string += '\n'
 
-                return_string += '- {name} [{health}] {current}/{total} HP'.format(name=(poke.name).upper(), current=round(poke.current_health, 2), total=poke.max_health, health=current_health_bar)
-                if self.pokemon_list[self.currently_active].name == poke.name:
-                    return_string += ' *currently active*'
-                return_string += '\n'
-
-
-        return_string += '\nPotions remaining: {pots}'.format(pots=self.potions)
-
-        return return_string 
-
-
-
-
-
-#charmander.lose_health(20)
-#May.use_potion()
-#May.use_potion()
-#May.use_potion()
-#sputnicc.attack(Saph)
-
-
-
-
-
+        return_string += '\n\n------------------- Items -------------------\n'
+        
+        return_string += 'Poke Coins: 0\n' # Coins
+        return_string += '\n---Potions remaining: {pots}'.format(pots=self.potions) # Potions
+        return_string += '\n---Pokeballs remaining: 0' # Pokeballs
+        return_string += '\n\n------------------- Trainer {name} Summary ------------------- \n'.format(name=(self.name).upper())
+        
+        return return_string
